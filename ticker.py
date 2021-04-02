33ab627ccbbe0e1fd9b5ffd09483b1b01d3a5ca0
sampot209 <81716581+sampot209@users.noreply.github.com>
2021-04-02 15:50:33 +0100

    Update ticker.py

--- ticker.py
+++ ticker.py
@@ -109,11 +109,11 @@ class Ticker(Frame):
             font_price.LoadFont('fonts/5x8.bdf')
 
         # Draw the elements on the canvas
-        price_text = asset["price"]
-        crypto_symbol = asset["symbol"]
-        crypto_symbol = crypto_symbol.upper()
-        price_text = price_text.split('.')[0]
-        price_text = price_text[1:-1] + 'K'
+        #price_text = asset["price"]
+        #crypto_symbol = asset["symbol"]
+        #crypto_symbol = crypto_symbol.upper()
+        #price_text = price_text.split('.')[0]
+        #price_text = price_text[1:-1] + 'K'
         graphics.DrawText(canvas, font_symbol, 0, 6, main_color, crypto_symbol)
         graphics.DrawText(canvas, font_price, 0, 26, main_colour, price_text)
         graphics.DrawText(
